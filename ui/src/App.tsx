b17005e77f240c01fde591bb7c117695516d9788
guigou <guigoucrypto@gmail.com>
2025-02-27 19:22:08 +0100

    mainnet version

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -7,15 +7,13 @@ import {
 } from "./ParticipationWorkflow";
 import {Synchronisation} from "./Synchronisation";
 
-const rpcShibuya = "wss://rpc.shibuya.astar.network";
-const managerAddress = "acKRTkuTsGESFmsy3wcvWTyfRW6xL4QY1gMhjNVtD5yUB7d";
-const registration1Address ="bSm4f7WjbxFMbo4fRUGw7oHvva65P8m8jCqedFsXAwUJx7V";
-const rpcMoonbase = "https://rpc.api.moonbase.moonbeam.network";
-const registration2Address = "0x987461a5eF325f9f217D2b777CeDCf3b9c4D62d5";
-const rpcMinato = "https://rpc.minato.soneium.org";
-const registration3Address = "0x04d884675E5790721cb5F24D41D460E921C08f17";
+const rpcAstar = "wss://rpc.astar.network";
+const managerAddress = "Y4j9j5EsehgPAjL6HHMMjHU9EhCdwrADazXEQdhFciG4uR9";
+const registrationAstar ="ZsaaL58Adg7k1xT1EumK575H6GymJ214kvRk8NxKouTWqnC";
+const rpcSoneium = "https://rpc.soneium.org";
+const registrationSoneium = "0xB2196C9B95BD3cdC799eb89f856895aEDbd649bB";
 const rpcPhala = "wss://poc6.phala.network/ws";
-const pinkContractAddress = "0x269bdbdef7285a1bdba7f68c79c1165705f20508e5e7c545b4fd10a6f345523e";
+const pinkContractAddress = "0xa41292a45bb4103c72796409d557416cc259ffb420a94fb433ae4c942fdc1218";
 
 export default function App() {
   return (
@@ -23,17 +21,15 @@ export default function App() {
       <header className="App-header">
         <svg width="1000" height="1000">
           <LegendManagerWorkflow cx={0}/>
-          <ManagerWorkflow cx={250} rpc={rpcShibuya} address={managerAddress} chain={"Astar testnet"}
-                           explorer={"https://shibuya.subscan.io/wasm_contract/"}
+          <ManagerWorkflow cx={250} rpc={rpcAstar} address={managerAddress} chain={"Astar"}
+                           explorer={"https://astar.subscan.io/wasm_contract/"}
           />
-          <ParticipationWorkflowWasm cx={400} rpc={rpcShibuya} address={registration1Address} chain={"Astar testnet"}
-                                     explorer={"https://shibuya.subscan.io/wasm_contract/"}/>
-          <ParticipationWorkflowEvm cx={500} rpc={rpcMoonbase} address={registration2Address} chain={"Moonbeam test"}
-                                    explorer={"https://moonbase.moonscan.io/address/"}/>
-          <ParticipationWorkflowEvm cx={600} rpc={rpcMinato} address={registration3Address} chain={"Soneium testnet"}
-                                    explorer={"https://soneium-minato.blockscout.com/address/"}/>
+          <ParticipationWorkflowWasm cx={500} rpc={rpcAstar} address={registrationAstar} chain={"Astar"}
+                                     explorer={"https://astar.subscan.io/wasm_contract/"}/>
+          <ParticipationWorkflowEvm cx={600} rpc={rpcSoneium} address={registrationSoneium} chain={"Soneium"}
+                                    explorer={"https://soneium.blockscout.com/address/"}/>
           <LegendParticipationWorkflow cx={700}/>
-          <Synchronisation rpcManagerContract={rpcShibuya} addressManagerContract={managerAddress}
+          <Synchronisation rpcManagerContract={rpcAstar} addressManagerContract={managerAddress}
                            rpcCommunicatingContract={rpcPhala} addressCommunicatingContract={pinkContractAddress}
                            explorerCommunicatingContract={"https://phala.subscan.io/wasm_contract/"}/>
         </svg>
